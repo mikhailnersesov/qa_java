79146d7aa66beb8896248145d9d25cc2a2b4df52
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2023-12-27 14:29:52 +0100

    code cleaned up

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,11 +1,8 @@
 package com.example;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
@@ -15,19 +12,18 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class LionTest{
+public class LionTest {
 
 
     @Mock
-     Animal animal;
+    Animal animal;
     @Mock
     Feline feline;
 
 
-
     @Test
     public void testGetKittens() {
-        Lion lion = new Lion(feline,animal);
+        Lion lion = new Lion(feline, animal);
         int expectedFood = 3;
         when(feline.getKittens()).thenReturn(expectedFood);
         int actualFood = lion.getKittens();
@@ -37,25 +33,28 @@ public class LionTest{
 
     @Test
     public void testGetFoodMale() throws Exception {
-        Lion lion = new Lion("Самец",feline,animal);
+        Lion lion = new Lion("Самец", feline, animal);
         List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
         when(animal.getFood("Хищник")).thenReturn(expectedFood);
         List<String> actualFood = lion.getFood();
         assertEquals(expectedFood, actualFood);
     }
+
     @Test
     public void testGetFoodFemale() throws Exception {
-        Lion lion = new Lion("Самка",feline,animal);
+        Lion lion = new Lion("Самка", feline, animal);
         List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
         when(animal.getFood("Хищник")).thenReturn(expectedFood);
         List<String> actualFood = lion.getFood();
         assertEquals(expectedFood, actualFood);
     }
-    @Test(expected=Exception.class)
+
+    @Test(expected = Exception.class)
     public void testGetFoodThirdGender() throws Exception {
-        Lion lion = new Lion("Третий пол",feline,animal);
+        Lion lion = new Lion("Третий пол", feline, animal);
     }
-    @Test(expected=Exception.class)
+
+    @Test(expected = Exception.class)
     public void testGetFoodThirdGender2() throws Exception {
         Lion lion = new Lion("Третий пол");
     }
