f586ca8340227533d75e796d292e547cdabef457
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2023-12-28 20:32:12 +0100

    AnimalTest.java tests renamed according to unit-test naming conventions

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -8,13 +8,13 @@ import static org.junit.Assert.assertEquals;
 public class AnimalTest {
 
     @Test(expected = Exception.class)
-    public void testGetFoodThirdGender() throws Exception {
+    public void getFoodUnknownAnimalKindThrowsException() throws Exception {
         Animal animal = new Animal();
         animal.getFood("Неизвестный вид");
     }
 
     @Test
-    public void testGetFamily() {
+    public void getFamilyDefaultNameShowsOk() {
         Animal animal = new Animal();
         String actualFamilyName = animal.getFamily();
         String expectedFamilyName = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
